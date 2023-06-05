1759c74cbfdd7003795045d87d464339de10f9b0
unknown <ashkan.codecman.01@gmail.com>
2023-06-05 19:44:35 +0330

    --

--- public_html/pages/search/search.php
+++ public_html/pages/search/search.php
@@ -2,9 +2,9 @@
 if ($_SERVER['SERVER_NAME'] == 'park-wash.ir') {
     echo 'in server';
     $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbName = "kprice";
+    $username = "umwganki_root";
+    $password = "@!A1a9s1s9";
+    $dbName = "umwganki_kprice";
 } elseif ($_SERVER['SERVER_NAME'] == 'localhost') {
     $servername = "localhost";
     $username = "root";
