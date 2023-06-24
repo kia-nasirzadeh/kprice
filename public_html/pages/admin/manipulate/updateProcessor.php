382d2493ea98196cc6a4cfc740b0513ec51d212d
unknown <ashkan.codecman.01@gmail.com>
2023-06-24 21:17:02 +0330

    first commit for debug

--- public_html/pages/admin/manipulate/updateProcessor.php
+++ public_html/pages/admin/manipulate/updateProcessor.php
@@ -2,4 +2,5 @@
 require __DIR__ . DIRECTORY_SEPARATOR . './../../../config.php';
 require_once __DIR__ . './../../../libs/dbhandler.php';
 $dbhandler = new DbHandler();
-echo $dbhandler->updateRecord('fullname', $_POST['fullname'], $_POST['group'], $_POST['subgroup'], $_POST['content']);
+echo ($_POST['content']);
+//echo $dbhandler->updateRecord('fullname', $_POST['fullname'], $_POST['group'], $_POST['subgroup'], $_POST['content']);
